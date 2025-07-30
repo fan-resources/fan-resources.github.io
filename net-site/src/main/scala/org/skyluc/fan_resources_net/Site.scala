b568afc4ada5f164ec65b01d223fb5e44fa42c45
Luc Bourlier <lb@skyluc.org>
2025-07-30 13:31:05 +0000

    Google search console validation

--- net-site/src/main/scala/org/skyluc/fan_resources_net/Site.scala
+++ net-site/src/main/scala/org/skyluc/fan_resources_net/Site.scala
@@ -5,7 +5,7 @@ import org.skyluc.fan_resources.html.Url
 object Site {
   val DOMAIN_NAME_FR = "fan-resources.net"
 
-  val GOOGLE_VERIFICATION_CODE = "DrE-ZbcyBV3lPatFCBja2O4ymKzfqFXDZjkfkTpvY_8"
+  val GOOGLE_VERIFICATION_CODE = "5DUf4g9lYSa_jzwy0JIrwsfTppM2uM5culgvgkbXj7U"
   val MICROSOFT_VERIFICATION_CODE = "B6C2BBE1BBDED01F740330EB10DEAEF8"
 
   def absoluteUrl(url: Url): Url =
